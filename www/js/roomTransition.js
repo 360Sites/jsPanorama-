867d155837a43aa47d81c52638103c65fd5160df
Ashot <mnatsakanyan.ashot@gmail.com>
2014-04-12 23:37:32 +0400

    inAlfa outAlfa

--- www/js/roomTransition.js
+++ www/js/roomTransition.js
@@ -1,3 +1,15 @@
 var RoomTransition = (function(){
 
+    function inAlfa(panorama) {
+
+    }
+    function outAlfa (panorama) {
+
+    }
+
+
+    return {
+        inAlfa : inAlfa,
+        outAlfa : outAlfa
+    };
 })();
